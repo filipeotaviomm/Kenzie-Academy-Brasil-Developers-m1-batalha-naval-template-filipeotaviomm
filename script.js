bc9bf55ead7e6de72858c2140b3a2befc7df7bee
Filipe <filipeotaviomm@gmail.com>
2023-04-28 16:27:18 -0300

    primeiro envio

--- script.js
+++ script.js
@@ -7,6 +7,36 @@ const seaBoard = [
   [".", ".", ".", ".", "."],
 ];
 
-/*
-  Desenvolva seu código logo abaixo
-*/
+function allocateShips(shipPositions, initialBoard) {
+  for (i = 0; i < shipPositions.length; i++) {
+    const posicaoEscolhida = shipPositions[i];
+    // console.log(posicaoEscolhida);
+    initialBoard[posicaoEscolhida[0]][posicaoEscolhida[1]] = "S";
+  }
+  // console.log(initialBoard);
+  return initialBoard;
+}
+
+function checkGuesses(guesses, mountedBoard) {
+  for (i = 0; i < guesses.length; i++) {
+    const posicaoS = guesses[i];
+    // console.log(posicaoS);
+    if (mountedBoard[posicaoS[0]][posicaoS[1]] == "S") {
+      mountedBoard[posicaoS[0]][posicaoS[1]] = "X";
+    }
+  }
+  return mountedBoard;
+}
+
+function checkWinCondition(guessedBoard) {
+  for (i = 0; i < guessedBoard.length; i++) {
+    for (j = 0; j < guessedBoard[i].length; j++) {
+      const posicoes = guessedBoard[i][j];
+      // console.log(posicoes);
+      if (posicoes == "S") {
+        return false;
+      }
+    }
+  }
+  return true;
+}
